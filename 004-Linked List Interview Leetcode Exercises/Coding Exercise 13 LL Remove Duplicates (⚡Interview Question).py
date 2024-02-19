65f0475d5cb07b7f7daec7f6c014e0ef23e49bc3
michaelorina <orinamichael4@gmail.com>
2024-02-19 12:34:01 +0300

    Coding Exercise 13 LL Remove Duplicates (⚡Interview Question)

---
+++ 004-Linked List Interview Leetcode Exercises/Coding Exercise 13 LL Remove Duplicates (⚡Interview Question).py
@@ -0,0 +1,13 @@
+def remove_duplicates(self):
+    values = set()
+    previous = None 
+    current = self.head 
+
+    while current:
+        if current.value in values:
+            previous.next = current.next
+            self.length -= 1
+        else:
+            values.add(current.value)
+            previous = current
+        current = current.next

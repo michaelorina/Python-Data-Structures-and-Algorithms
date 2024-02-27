6edf74565c8383bf2bb621a2d180c7ac0e776012
michaelorina <orinamichael4@gmail.com>
2024-02-27 14:59:16 +0300

    005 DLL Pop First

---
+++ 005-Doubly Linked List/005 DLL Pop First.py
@@ -0,0 +1,13 @@
+def pop_first(self):
+    if self.length == 0:
+        return None
+    temp = self.length
+    if self.length == 1:
+        self.head = None
+        self.tail = None
+    else:
+        self.head = self.head.next
+        self.head.prev = None 
+        temp.next = None 
+    self.length -= 1
+    return temp     

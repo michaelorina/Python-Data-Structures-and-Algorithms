d3198ba3b1efc88ee9b1972ad80a304aa126cd10
michaelorina <orinamichael4@gmail.com>
2024-03-04 12:03:14 +0300

    007 Queue Enqueue

---
+++ 007 - Stack & Queues/007 Queue Enqueue.py
@@ -0,0 +1,10 @@
+def enqueue(self, value):
+    new_node = Node(value)
+    if self.first == None:
+        self.first = new_node
+        self.last = new_node
+    else:
+        self.last.next = new_node
+        self.last = new_node
+    self.length += 1
+
